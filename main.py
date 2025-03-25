0cbc60bb822472034614beb4d1aa144bf61f2576
Filip Markovic <filip98122@gmail.com>
2025-03-25 10:40:02 +0100

    No boulder offset use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from loader import *
+from map import *
 def ens(file_data):
     f=Fernet(keyE)
     encrypted_data1=json.dumps(file_data).encode('utf-8')
@@ -81,19 +82,8 @@ def get_angle(dx,dy):
     angle_rad = math.atan2(dy, dx)
     angle_deg = math.degrees(angle_rad)
     return angle_deg
-l_terrain=[
-    "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
-    "wddddddddddddgdddddddddddddddddddddddddddddddddddddddddddddddddddddddw",
-    "wddgdgdddgdddgdddddddddddddgggddddgdddddddggddddgddgdddddddddddddddddw",
-    "wddgggdddgdddddddddddddddddgddgddgdgdddddgddgdddgdgddddddddddddddddddw",
-    "wddgdgdddgdddbd     dddddddgggddgdddgddddgddddddggdddddddddddddddddddw",
-    "wddddddpbdddddd     dddddddggdddgdddgddddgddddddggdddddddddddddddddddw",
-    "wdddddddddddddd     dddddddgdgdddgdgdddddgddgdddgdgddddddddddddddddddw",
-    "wdddddddddddddd     dddddddgddgdddgdddddddggddddgddgdddddddddddddddddw",
-    "wddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddw",
-    "wddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddw",
-    "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww"
-]
+
+
 vremepre=time.time()
 najvecivreme=0
 class Terrain:
@@ -113,7 +103,7 @@ class Terrain:
                     s.img=loaded[4]
                 
                 if l_terrain[i][j]=="p":
-                    window.blit(s.img,(j*(HEIGHT/9)+((HEIGHT/9-HEIGHT/12)/2),i*(HEIGHT/9)))
+                    window.blit(s.img,(player.x+((HEIGHT/9-HEIGHT/12)/2),player.y))
                 else:
                     if j*(HEIGHT/9)+offsetx>-1+-HEIGHT/9 and j*(HEIGHT/9)+offsetx<WIDTH and i*(HEIGHT/9)+offsety>-1+-HEIGHT/9 and i*(HEIGHT/9)+offsety<=HEIGHT and s.img!="":
                         window.blit(s.img,(j*(HEIGHT/9)+offsetx,i*(HEIGHT/9)+offsety))
@@ -127,6 +117,40 @@ offsety=0
 camera_speed=HEIGHT/9
 between=0
 SVAKIH30=0
+
+class Boulder:
+    def __init__(s,x,y):
+        s.x=x
+        s.y=y
+        s.Falling=False
+        s.time=13
+    def move(s,map):
+        terraincheck=map[int(s.y/(HEIGHT/9))+1][int(s.x/(HEIGHT/9))]
+        if terraincheck=="d" or terraincheck=="w" or terraincheck=="g":
+            s.Falling=False
+        elif terraincheck==" ":
+            if s.time==0:
+                map[int(s.y/(HEIGHT/9))+1][int(s.x/(HEIGHT/9))]="b"
+                map[int(s.y/(HEIGHT/9))][int(s.x/(HEIGHT/9))]=" "
+                s.y+=(HEIGHT/9)
+                s.time=13
+        elif terraincheck=="p":
+            if s.Falling==True:
+                if s.time==0:
+                    pass
+                    #die
+            else:
+                pass
+        elif terraincheck=="b":
+                if s.Falling==True:
+                    s.Falling=False
+                else:
+                    if s.time==0:
+                        pass
+                        #tumble
+        if s.time>0:
+            s.time-=1
+                    
 class Player:
     def __init__(s,x,y):
         s.x=x
@@ -136,21 +160,26 @@ class Player:
         terraincheck=map[int(s.y/(HEIGHT/9)-(offsety/(HEIGHT/9)))][int(s.x/(HEIGHT/9)-offsetx/(HEIGHT/9))]
         goback=True
         if terraincheck=="d" or terraincheck==" ":
-            map[int(s.y/(HEIGHT/9)-(offsety/(HEIGHT/9)))] = replace_at(map[int(s.y/(HEIGHT/9)-(offsety/(HEIGHT/9)))], [int(s.x/(HEIGHT/9)-offsetx/(HEIGHT/9))], " ")
+            map[indexpre1][indexpre2]=" "
+            map[int(s.y/(HEIGHT/9)-(offsety/(HEIGHT/9)))][int(s.x/(HEIGHT/9)-offsetx/(HEIGHT/9))]="p"
             goback=False
         if terraincheck=="g":
             diamonds+=1
             goback=False
-            map[int(s.y/(HEIGHT/9)-(offsety/(HEIGHT/9)))] = replace_at(map[int(s.y/(HEIGHT/9)-(offsety/(HEIGHT/9)))], [int(s.x/(HEIGHT/9)-offsetx/(HEIGHT/9))], " ")
+            map[indexpre1][indexpre2]=" "
+            map[int(s.y/(HEIGHT/9)-(offsety/(HEIGHT/9)))][int(s.x/(HEIGHT/9)-offsetx/(HEIGHT/9))]="p"
         return map,goback
 def replace_at(s, index, new_char):
-    return s[:index[0]] + new_char + s[index[0]+1:]
-
+    s[index[0]]=new_char
+    return s
+l_boulders=[]
 
 for i in range(len(l_terrain)):
     for j in range(len(l_terrain[i])):
         if l_terrain[i][j]=="p":
             player=Player(j*(HEIGHT/9),i*(HEIGHT/9))
+        if l_terrain[i][j]=="b":
+            l_boulders.append(Boulder(j*(HEIGHT/9),i*(HEIGHT/9)))
 
 
 
@@ -165,37 +194,64 @@ while True:
     for event in events:
         if event.type == pygame.QUIT:
             break
-        if between<=0:
-                
-                x = joystick.get_axis(0)
-                y = joystick.get_axis(1)  # Invert Y for normal coordinate system
-
-                magnitude = math.hypot(x, y)
-                if magnitude > 0.2:  # Deadzone to avoid jitter
-                    angle = math.degrees(math.atan2(-y, x))
-                    angle%=360
-                    preoffset=[offsetx,offsety]
-                    if angle<=45 or angle>315:
-                        offsetx-=camera_speed
-                    elif angle>45 and angle<=135:
-                        offsety+=camera_speed
-                    elif angle<=225 and angle>135:
-                        offsetx+=camera_speed
-                    else:
-                        offsety-=camera_speed
+    for i in range(len(l_boulders)):
+        l_boulders[i].move(l_terrain,player)
+    if between<=0:
+        if pygame.joystick.get_count()>0:
+            try:
+                a=joystick.get_axis(0)
+            except:
+                joystick=pygame.joystick.Joystick(0)
+                joystick.init()
+            x = joystick.get_axis(0)
+            y = joystick.get_axis(1)  # Invert Y for normal coordinate system
+            magnitude = math.hypot(x, y)
+            if magnitude > 0.4:  # Deadzone to avoid jitter
+                angle = math.degrees(math.atan2(-y, x))
+                angle%=360
+                indexpre1=int(player.y/(HEIGHT/9)-(offsety/(HEIGHT/9)))
+                indexpre2=int(player.x/(HEIGHT/9)-offsetx/(HEIGHT/9))
+                preoffset=[offsetx,offsety]
+                if angle<=45 or angle>315:
+                    offsetx-=camera_speed
+                elif angle>45 and angle<=135:
+                    offsety+=camera_speed
+                elif angle<=225 and angle>135:
+                    offsetx+=camera_speed
+                else:
+                    offsety-=camera_speed
+                between=12
+                l_terrain,goback=player.move(l_terrain)
+                if goback:
+                    offsetx=preoffset[0]
+                    offsety=preoffset[1]
+        else:
+            preoffset=[offsetx,offsety]
+            went=False
+            if keys[pygame.K_d]:
+                offsetx-=camera_speed
+                went=True
+            elif keys[pygame.K_s]:
+                offsety-=camera_speed
+                went=True
+            elif keys[pygame.K_a]:
+                offsetx+=camera_speed
+                went=True
+            elif keys[pygame.K_w]:
+                offsety+=camera_speed
+                went=True
+            if went==True:
+                l_terrain,goback=player.move(l_terrain)
+                if goback:
+                    offsetx=preoffset[0]
+                    offsety=preoffset[1]
+                else:
                     between=12
-                    l_terrain,goback=player.move(l_terrain)
-                    if goback:
-                        offsetx=preoffset[0]
-                        offsety=preoffset[1]
     if keys[pygame.K_ESCAPE]:
         break
     if between>0:
         between-=1
     terrain.draw(l_terrain)
-    if keys[pygame.K_b]:
-        breakpoint()
-        
     pygame.display.update()
     clock.tick(60)
     SVAKIH30+=1
